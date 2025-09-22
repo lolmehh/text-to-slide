7bc201366cbd62476f678d7911a2f59413e25586
ICEturbo <m.pijnenburg2008@gmail.com>
2025-09-22 16:15:54 +0200

    gui maken voor presentatie

--- GUI.py
+++ GUI.py
@@ -1,34 +1,51 @@
 import pygame
 import pygame_gui
 
-
 pygame.init()
 
-pygame.display.set_caption('Quick Start')
-window_surface = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Text to slide')
+window_surface = pygame.display.set_mode((500, 500))
 
 background = pygame.Surface((800, 600))
-background.fill(pygame.Color("#D49D9D"))
+background.fill(pygame.Color("#FFFFFF"))
 
 manager = pygame_gui.UIManager((800, 600))
 
-hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
-                                             text='Say Hello',
-                                             manager=manager)
+# Bestaande knoppen
+
+uitzetknop = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((0, 425), (500, 75)),
+    text='Zet tekst om naar powerpoint presentatie.',
+    manager=manager
+)
+
+# Kleur voor uitzetknop
+uitzetknop.colours['normal_bg'] = pygame.Color("#388328")
+uitzetknop.colours['hovered_bg'] = pygame.Color("#46a442")
+uitzetknop.colours['active_bg'] = pygame.Color("#2e7d32")
+uitzetknop.rebuild()
+
+tekstbox = pygame_gui.elements.UITextEntryLine(
+    relative_rect=pygame.Rect((0, 0), (500, 425)),
+    manager=manager
+)
+
 
 clock = pygame.time.Clock()
 is_running = True
 
 while is_running:
-    time_delta = clock.tick(60)/1000.0
+    time_delta = clock.tick(60) / 1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
 
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == hello_button:
-                #Als de knop is ingedrukt gebeurd dit
-                print("bob is cool")
+
+            if event.ui_element == uitzetknop   :
+                # Tekst uit het tekstvak ophalen en printen
+                print("Tekstbox inhoud:", tekstbox.get_text())
+                print("De tekst wordt nu omgezet naar een powerpointpresentatie")
 
         manager.process_events(event)
 
