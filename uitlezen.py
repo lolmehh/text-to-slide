14213c5c77a049883640e969ee208cf1c44bc368
sadiyehmahmoud17-cloud <sadiyehmahmoud17@gmail.com>
2025-09-29 15:46:01 +0200

    Update uitlezen.py

--- uitlezen.py
+++ uitlezen.py
@@ -3,21 +3,12 @@ def bestandlezen(abc):
     bestand =  "De banaan wordt opgegeten door Bob"
     return bestand
 
-
-
-
-
-#def uitlezen(
-    #from gui import bestand
-
-
-#):
-#    f = open(bestand, "r")
-    #text = f.read()
-    #print(text)
-    #if '.' in text:
-        #position = text.find('.')
-        #sentence = ""
-        #print(position)
-        #importtekst = text[0:position+1]
-        #return importtekst
+   f = open(bestand, "r")
+    text = f.read()
+    print(text)
+    if '.' in text:
+        position = text.find('.')
+        sentence = ""
+        print(position)
+        importtekst = text[0:position+1]
+        return importtekst
